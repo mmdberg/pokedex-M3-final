b06af45992be37ea612e4487f642221d5c6fb4ab
Maddy Dannenberg <mmdberg@gmail.com>
2018-04-12 09:31:18 -0600

    Set up test file for apiCalls and mockData file

---
+++ src/apiCalls/index.test.js
@@ -0,0 +1,6 @@
+import * as api from './index';
+import { mockPokemonTypeData } from '../mockData';
+
+describe('Api calls', () => {
+  
+})
---
+++ src/mockData/index.js
@@ -0,0 +1,13 @@
+export const mockPokemonTypeData = [
+  {
+    "id": "1",
+    "name": "normal",
+    "pokemon": [
+      "16",
+      "17",
+      "18",
+      "19",
+      "20"
+    ]
+  },
+]
